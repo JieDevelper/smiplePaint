ed78e6464802e2eaf955d003616ee652d7c4bccf
ZhangJie <zhangjie@baiwenbao.com>
2016-03-19 16:53:17 +0800

    Initial Commit

---
+++ PaintSimple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaintSimple
+//
+//  Created by ZhangJie on 3/19/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaintSimple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaintSimple
+//
+//  Created by ZhangJie on 3/19/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
